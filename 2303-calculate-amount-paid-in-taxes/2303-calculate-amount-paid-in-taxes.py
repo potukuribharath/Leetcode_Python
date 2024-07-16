3153082aa0eb19a451d2e485b282785164522afb
potukuribharath <103629063+potukuribharath@users.noreply.github.com>
2024-07-16 22:49:46 +0530

    Time: 72 ms (46.75%), Space: 16.6 MB (32.73%) - LeetHub

---
+++ 2303-calculate-amount-paid-in-taxes/2303-calculate-amount-paid-in-taxes.py
@@ -0,0 +1,38 @@
+class Solution:
+    def calculateTax(self, brackets: List[List[int]], income: int) -> float:
+        if income==0:
+            return 0
+        else:
+            
+            tax=0
+            if len(brackets)==1:
+                tax+=income*(brackets[0][1])/100
+                return tax
+            if income>brackets[0][0]:
+                tax+=brackets[0][0]*(brackets[0][1])/100
+                income-=brackets[0][0]
+            else:
+                tax+=income*(brackets[0][1])/100
+                return tax
+            print(tax)
+            for i in range(1,len(brackets)):
+                
+                print(tax)
+                if income > 0:
+                    if income>=brackets[i][0]-brackets[i-1][0]:
+                        rem=brackets[i][0]-brackets[i-1][0]
+                        tax+=rem*(brackets[i][1])/100
+                        income-=rem
+                       
+
+                    else:
+                        tax+=income*(brackets[i][1])/100
+                        break
+                        
+                    
+                    
+                            
+            if tax<0:
+                return -(tax)
+            return tax
+        
